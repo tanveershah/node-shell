6c3b56e9e34064f9ceb9d43295c40224ab6b0e6f
taveershah <tanvirhussein@gmail.com>
2020-08-07 12:29:46 -0400

    streamline with nodemon done

--- bash.js
+++ bash.js
@@ -1,8 +1,12 @@
-process.stdout.write('prompt > ');
+process.stdout.write("prompt > ");
 
-process.stdin.on('data', (data) => {
+process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-})
+  if (cmd === "pwd") {
+    console.log(process.cwd());
+  }
+
+  process.stdout.write("You typed: " + cmd);
+  process.stdout.write("\nprompt > ");
+});
